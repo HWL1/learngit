329c963f0dbe5f6dcf5c378e74ba30405effe873
Your Name <email@example.com>
2018-11-27 10:00:19 +0800

    HWL@DBmap修改

--- src/main/java/com/alibabacloud/polar_race/engine/common/engineDao/DBmap.java
+++ src/main/java/com/alibabacloud/polar_race/engine/common/engineDao/DBmap.java
@@ -1,21 +1,27 @@
 package com.alibabacloud.polar_race.engine.common.engineDao;
 
 import com.alibabacloud.polar_race.engine.common.exceptions.EngineException;
+import com.alibabacloud.polar_race.engine.common.logFileAOF.MyLoggerFactory;
+import com.alibabacloud.polar_race.engine.common.util.UtilClass;
 
 import java.util.concurrent.ConcurrentHashMap;
 
 public class DBmap {
-
+    private static  MyLoggerFactory log = new  MyLoggerFactory(DBmap.class);
     private static volatile ConcurrentHashMap<byte[],byte[]> DBMap;
     public byte[] read(byte[] key)throws EngineException {
-
-        return DBMap.get(key);
-
+  try {
+      return DBMap.get(key);
+  }catch (Exception e){
+      System.out.println("key:为空" + UtilClass.toString(key));
+      return null;
+  }
     }
 
     public void write(byte[] key,byte[] val)throws EngineException {
-
+        System.out.println("write   key:" +key[0]+ "    val:"+val[0]);
         DBMap.put(key, val);
+        log.myLogger(""+DBMap.get(key));
     }
 
 

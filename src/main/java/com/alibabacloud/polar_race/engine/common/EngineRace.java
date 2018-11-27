45b27102791579a2a1711d955fbf97d9c6e1afb0
Your Name <email@example.com>
2018-11-27 10:33:07 +0800

    HWL@修改

--- src/main/java/com/alibabacloud/polar_race/engine/common/EngineRace.java
+++ src/main/java/com/alibabacloud/polar_race/engine/common/EngineRace.java
@@ -3,8 +3,10 @@ package com.alibabacloud.polar_race.engine.common;
 import com.alibabacloud.polar_race.engine.common.engineDao.DBmap;
 import com.alibabacloud.polar_race.engine.common.engineDao.SingletonDBMap;
 import com.alibabacloud.polar_race.engine.common.exceptions.EngineException;
+import com.alibabacloud.polar_race.engine.common.logFileAOF.MyLoggerFactory;
 
 public class EngineRace extends AbstractEngine {
+	private static MyLoggerFactory log = new  MyLoggerFactory(EngineRace.class);
 	DBmap singletonDBMap = SingletonDBMap.getInstance();
 	@Override
 	public void open(String path) throws EngineException {
@@ -12,6 +14,9 @@ public class EngineRace extends AbstractEngine {
 	
 	@Override
 	public void write(byte[] key, byte[] value) throws EngineException {
+		if(singletonDBMap == null)
+			log.myLogger("singletonDBMap = "+singletonDBMap);
+
 		singletonDBMap.write(key,value);
 	}
 
